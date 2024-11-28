47406c4b3b992275f88c7965b401e5b6f3f4ede8
NopksForge <noppasan.ksj@gmail.com>
2024-11-28 10:05:28 +0700

    🔇 remove log

--- frontend/src/app/components/UserCard.tsx
+++ frontend/src/app/components/UserCard.tsx
@@ -9,7 +9,6 @@ interface UserCardProps {
 }
 
 const formatDate = (dateString: string) => {
-  console.log('Incoming date string:', dateString); // Debug log
   const date = new Date(dateString);
   const day = date.getDate().toString().padStart(2, '0');
   const month = date.toLocaleString('en-US', { month: 'short' });
